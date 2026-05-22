39611e9ec3c0dbb0f26e0891db6e0fec10f243de
Supratick Mondal <sonumondal5006@gmail.com>
2026-05-22 13:53:59 +0530

    Counter + Write + Read file

--- easy/counter2.js
+++ easy/counter2.js
@@ -11,19 +11,82 @@ Without using setInterval, try to code a counter in Javascript. There is a hint 
 */
 
 
-let c = 0;
+// let c = 0;
 
-function Counter(){
+// function Counter(){
    
-  c++;
-  console.log(c);
+//   c++;
+//   console.log(c);
 
-  // Counter should be called after 1 second
-  setTimeout(Counter,1000);
-
-
-
-}
+//   // Counter should be called after 1 second
+//   setTimeout(Counter,1000);
 
 
-Counter()
+
+// }
+
+
+// Counter()
+
+// console.log(1);
+
+
+// async function yoo(params) {
+//   console.log(3);
+// }
+
+// setTimeout(()=>{
+//   yoo()
+// },0);
+
+// console.log(2);
+
+import { error } from "node:console";
+import fs, { readFile } from "node:fs"
+
+
+fs.readFile("./A.txt",'utf8',(err,data)=>{
+   if(err){
+    console.log(err)
+    return
+   }
+
+  //  const hasWhitespace = /\s/.test(data);
+   
+  //  Only remove whiteSpaces : data.trim();
+  //  Replace white Spaces with " " one white space 
+   const ans  = data.replace(/\s+/g,' ').trim();
+
+
+  //  How you write a file : 
+  // (./filename.txt,data you want to write, Format)
+   fs.writeFileSync("./example.txt",ans,'utf-8')
+   
+
+   console.log(ans)
+})
+
+/*
+
+Without : utf-8 
+We get something we call as : 
+<Buffer 0d 0a 48 69 20 49 20 61 6d 20 43 52 37>
+*/
+
+
+// ReadFile();
+
+/*
+fs.readFile('filename','utf-8',(data,err)=> {
+
+    //  if error => console.log()
+    
+    // data.replace(/\s/g+,' ').trim();
+    // Whitespread spaces ko replace it with ' '(Single Space).trim() Removes the spaces . 
+
+    const ans = data.trim(); // removes the whitespaces.
+
+
+})
+
+*/
